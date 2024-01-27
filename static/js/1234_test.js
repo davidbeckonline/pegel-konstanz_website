5ac5b94b8e9664d2d87338a61bd3d4a2c95bc135
EC2 Default User <ec2-user@ip-172-31-7-53.eu-central-1.compute.internal>
2024-01-27 07:34:59 +0000

    change path for JS from current.json to currentdata.json

--- static/js/1234_test.js
+++ static/js/1234_test.js
@@ -1,6 +1,6 @@
 async function loadPegel() {
   try {
-    const response = await fetch("https://pegel-konstanz-for-website.s3.eu-central-1.amazonaws.com/json/current.json");
+    const response = await fetch("https://pegel-konstanz-for-website.s3.eu-central-1.amazonaws.com/json/currentdata.json");
     const current = await response.json();
     console.log(current);
     const currentLevel = current["current_level"];
